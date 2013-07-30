69b2148759f1bdfd5993c9bd5a87eeb157ed8ff6
gmamaladze <Duff0ssu@>
2013-07-30 23:13:42 +0200

    Adjuste assembly info

--- Gma.DataStructures.StringSearch.DemoApp/Properties/AssemblyInfo.cs
+++ Gma.DataStructures.StringSearch.DemoApp/Properties/AssemblyInfo.cs
@@ -1,38 +1,15 @@
 ﻿// This code is distributed under MIT license. Copyright (c) 2013 George Mamaladze
 // See license.txt or http://opensource.org/licenses/mit-license.php
 using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
 [assembly: AssemblyTitle("Gma.DataStructures.StringSearch.DemoApp")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyDescription("An Application to demonstrate .NET Data Structures for Substring Search, Auto-completion and Intelli-sense.")]
+[assembly: AssemblyCompany("George Mamaladze")]
 [assembly: AssemblyProduct("Gma.DataStructures.StringSearch.DemoApp")]
-[assembly: AssemblyCopyright("Copyright ©  2012")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
+[assembly: AssemblyCopyright("Copyright © George Mamaladze 2013")]
 [assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("dd3d4d0d-0bc4-4a0c-a8dd-76e70b5e9ee5")]
+[assembly: AssemblyVersion("1.0.1.*")]
+[assembly: AssemblyFileVersion("1.0.1.*")]
 
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
--- Gma.DataStructures.StringSearch/Properties/AssemblyInfo.cs
+++ Gma.DataStructures.StringSearch/Properties/AssemblyInfo.cs
@@ -3,39 +3,12 @@
 using System.Reflection;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-
-[assembly: AssemblyTitle("Gma.AdvancedDataStructures.Trie")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Licence Owner")]
-[assembly: AssemblyProduct("Gma.AdvancedDataStructures.Trie")]
-[assembly: AssemblyCopyright("Copyright © Licence Owner 2012")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
-
+[assembly: AssemblyTitle("Gma.DataStructures.StringSearch")]
+[assembly: AssemblyDescription(".NET Data Structures for Substring Search, Auto-completion and Intelli-sense.")]
+[assembly: AssemblyCompany("George Mamaladze")]
+[assembly: AssemblyProduct("Gma.DataStructures.StringSearch")]
+[assembly: AssemblyCopyright("Copyright © George Mamaladze 2013")]
 [assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-
 [assembly: Guid("d26f9135-08b5-4f5e-93c4-04707132110b")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.1.*")]
+[assembly: AssemblyFileVersion("1.0.1.*")]
