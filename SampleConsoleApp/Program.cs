3b6df9fdd39ffc3b86df7d6583c63b2e6814de74
OliBomby <olivierschipper@gmail.com>
2022-04-23 14:25:36 +0200

    moved stuff

--- SampleConsoleApp/Program.cs
+++ SampleConsoleApp/Program.cs
@@ -1,10 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.IO;
-using System.Linq;
+﻿using System.Diagnostics;
 using System.Text;
-using Gma.DataStructures.StringSearch;
 
 namespace Gma.DataStructures.StringSearch.SampleConsoleApp
 {
